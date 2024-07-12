a4cfe39e8c1baddee4cf61184b86b561cfd2fbfe
aleperfect <143966062+aleperfect@users.noreply.github.com>
2024-07-12 16:19:46 -0500

    SD

--- handler.js
+++ handler.js
@@ -1546,7 +1546,7 @@ global.dfail = (type, m, conn) => {
     restrict: '*[ ℹ️ ] Este comando fue desactivado por el propietario del bot.*',
   }[type];
   const aa = {quoted: m, userJid: conn.user.jid};
-  const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: msg, contextInfo: {externalAdReply: {title: '*[ ⚠ ] Advertencia*', body: 'INKABOT', thumbnail: imagen1, sourceUrl: 'https://www.instagram.com/inkaikos_ff'}}}}, aa);
+  const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: msg, contextInfo: {externalAdReply: {title: '*[ ⚠ ] Advertencia*', body: 'INKABOT', thumbnail: imagen1, sourceUrl: 'https://www.instagram.com/ale.izn_20/'}}}}, aa);
   if (msg) return conn.relayMessage(m.chat, prep.message, {messageId: prep.key.id});
 };
 
